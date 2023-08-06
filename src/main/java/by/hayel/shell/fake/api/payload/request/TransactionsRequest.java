245a98480c8150f2cde034f052bde882050f22ca
Ivan Hayel <i.hayel.dev@gmail.com>
2023-08-06 11:20:46 +0300

    Release v0.0.2

--- src/main/java/by/hayel/shell/fake/api/payload/request/TransactionsRequest.java
+++ src/main/java/by/hayel/shell/fake/api/payload/request/TransactionsRequest.java
@@ -1,16 +1,21 @@
 package by.hayel.shell.fake.api.payload.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@FieldDefaults(level = AccessLevel.PUBLIC)
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class TransactionsRequest implements Request {
-  String RequestID;
-  String ColcoCountryCode;
-  String FromSalesDateTime;
-  String ToSalesDateTime;
+  @JsonProperty("RequestID") String requestId;
+  @JsonProperty("ColcoCountryCode") String countryCode;
+  @JsonProperty("FromSalesDateTime") String fromSalesDateTime;
+  @JsonProperty("ToSalesDateTime") String toSalesDateTime;
 }
